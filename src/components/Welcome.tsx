6da45d1af9169a6f1542cd4c214764565433d615
Subaru Sakaguchi <subarusakaguchi@yahoo.com.br>
2022-09-27 19:30:15 -0300

    Styled components added

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export function Welcome() {
+  return (
+    <View>
+        <Text>React Native Configurado com sucesso</Text>
+    </View>
+  );
+}
